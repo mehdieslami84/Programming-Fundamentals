2134d552993ce3138e7d5309b5b9e6eb47c4c177
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-22 11:46:54 +0330

    Add question 5

---
+++ src/P005.java
@@ -0,0 +1,22 @@
+// Write a program that takes a number and prints its absolute value without using an if statement.
+
+import java.util.Scanner;
+
+public class P005 {
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+
+
+        System.out.print("pls enter Number 1 : ");
+        int n = input.nextInt(); //Enter n
+        
+        System.out.println("pls enter Number 2 : ");
+        int m = input.nextInt(); //Enter m
+
+         
+        int min = Math.min(n, m);
+
+        System.out.println("Min : " + min);
+
+    }//end of main
+}//end of class
