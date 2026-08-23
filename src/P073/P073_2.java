0668451a8fc1dcba8db11922f4744111940cb012
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-24 00:07:03 +0330

    Add question 073_2

---
+++ src/P073/P073_2.java
@@ -0,0 +1,30 @@
+package P073;
+
+import java.util.Scanner;
+
+public class P073_2 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("pls enter Number : ");
+
+        int n = input.nextInt();
+
+        for (int x = 1; x <= n; x++) {
+
+            for (int y = 1; y <= n - x; y++) {
+                System.out.print(" ");
+            }
+
+            for (int y = 1; y <= x; y++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }//end of main
+
+}//end of class
