b7c4dd41537b6815122a1fa5db124b57f532a32f
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-25 01:22:22 +0330

    Add question 089

---
+++ src/P089.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class P089 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter Number : ");
+        long n = input.nextInt();
+        String s = "";
+        long b = 0;
+        long p = 1;
+        while (n > 0) {
+            b = (n % 10) * p + b;
+            p *= 2;
+            n /= 10;
+        }
+        System.out.println(s);
+        while (b > 0) {
+            long r = b % 16;
+
+            if (r < 10) {
+                s = r + s;
+            } else {
+                s = (char) (r + 55) + s;
+            }
+
+            b = b / 16;
+        }
+        System.out.println(s);
+    }//end of main
+}//end of class
