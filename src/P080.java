b19b754fd004cd3f3debab6c1dfb81f55818cd4f
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-24 16:08:15 +0330

    Add question 080

--- src/P080.java
+++ src/P080.java
@@ -1,2 +1,30 @@
-public class P080 { 
-}
+
+import java.util.Scanner;
+
+public class P080 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int c = 0;
+        for (int x = 1; x <= 3; x++) {
+            System.out.print("Please Enter Number " + x + " : ");
+            int n = input.nextInt();
+            int sum = 0;
+
+            for (int y = 1; y < n; y++) {
+                if (n % y == 0) {
+                    sum += y;
+
+                    if (sum == x && x % 2 == 0) {
+                        System.out.println("its is right and even");
+                        c++;
+                    }
+                }
+
+            }
+
+        }
+        System.out.println(c);
+
+    }//end of main
+}//end of class
