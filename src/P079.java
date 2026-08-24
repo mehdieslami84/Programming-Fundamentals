d8f5d63e1e507135578da0363e8e17a257302d9b
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-24 16:05:24 +0330

    Add question 079

---
+++ src/P079.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class P079 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int c = 0;
+        for (int x = 1; x <= 3; x++) {
+            System.out.print("Please Enter Number " + x + " : ");
+            int n = input.nextInt();
+            int sum = 0;
+
+            for (int y = 1; y < n; y++) {
+                if (n % y == 0) {
+                    sum += y;
+
+                    if (sum == x) {
+                        System.out.println("its is right");
+                        c++;
+                    }
+                }
+
+            }
+
+        }
+        System.out.println(c);
+
+    }//end of main
+}//end of class
