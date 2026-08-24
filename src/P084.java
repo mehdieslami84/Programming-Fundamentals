eafe9bb2781b25b5dcc3625947d0c8db4dfd0024
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-24 18:49:07 +0330

    Add question 084

--- src/P084.java
+++ src/P084.java
@@ -5,16 +5,44 @@ public class P084 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int mul = 1;
+        for (int x = 1; x <= 2; x++) {
+            System.out.print("Please Enter Number " + x + " : ");
+            int n = input.nextInt();
+            int p = 1;
+            int d = 0;
+            while (n > 0) {
+                d = p * (n % 10) + d;
+                p = p * 8;
+                n /= 10;
+            }
 
-        System.out.print("Please Enter Number : ");
-        int n = input.nextInt();
-        int p = 1;
-        int d = 0;
-        while (n > 0) {
-            d = p * (n % 10) + d;
-            p = p * 2;
-            n /= 10;
+            sum += d;
+            mul *= d;
         }
-        System.out.println("Decimal is  : " + d);
+
+        int b = 0;
+        int p = 1;
+
+        while (sum > 0) {
+
+            b = b + (sum % 8) * p;
+            p = p * 10;
+            sum /= 8;
+        }
+        System.out.println("sum is : " + b);
+        b = 0;
+        p = 1;
+
+        while (mul > 0) {
+
+            b = b + (mul % 8) * p;
+            p = p * 10;
+            mul /= 8;
+        }
+
+        System.out.println("mul is : " + b);
     }
+
 }
