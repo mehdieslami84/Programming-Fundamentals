88466a01253bea2b566182c05300fcc397ffe5ef
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-24 19:00:23 +0330

    Add question 087

---
+++ src/P087.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class P087 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter Number : ");
+        int n = input.nextInt();
+        int p = 1;
+        int d = 0;
+        while (n > 0) {
+            d = p * (n % 10) + d;
+            p = p * 2;
+            n /= 10;
+        }
+        System.out.println("Decimal is  : " + d);
+
+        p = 1;
+        int b = 0;
+        while (d > 0) {
+            b = b + (d % 8) * p;
+            p *= 10;
+            d /= 8;
+
+        }
+        System.out.println(b);
+    }
+}
