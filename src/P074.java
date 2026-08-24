900d827625ef11bf81d0555e59605b98f314693b
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-24 14:19:28 +0330

    Add question 074

---
+++ src/P074.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class P074 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println();
+        for (int x = 1; x <= 10; x++) {
+            for (int y = 1; y <= 10; y++) {
+                if (x * y >= 1 && x * y <= 9) {
+                    System.out.print(" " + x * y + "  "); 
+                }else {
+                    System.out.print(x * y + "  ");
+                }
+            }
+            System.out.println("");
+        }
+
+    }// end of main
+}// end of class
