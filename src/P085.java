4d91fb25fd0a645f8bd818596fff485823b986ce
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-24 18:36:12 +0330

    Add question 085

--- src/P085.java
+++ src/P085.java
@@ -1,2 +1,20 @@
-public class P085 { 
+
+import java.util.Scanner;
+
+public class P085 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter Number : ");
+        int n = input.nextInt();
+        int p = 1;
+        int d = 0;
+        while (n > 0) {
+            d = p * (n % 10) + d;
+            p = p * 2;
+            n /= 10;
+        }
+        System.out.println("Decimal is  : " + d);
+    }
 }
