3e8b549161c77db00fa70c0ec73611531d11293d
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-26 21:55:08 +0330

    Add question 122

---
+++ src/P122.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+// Write a program that takes 5 numbers and prints them from last to first.
+public class P122 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int A[] = new int[20];
+        for (int x = 0; x < 20; x++) {
+            System.out.print("Enter Number " + (x + 1) + " : ");
+            A[x] = input.nextInt();
+        }
+        for (int x = 19; x >= 0; x--) {
+            System.out.print(A[x] + "  ");
+
+        }
+
+    }
+}
