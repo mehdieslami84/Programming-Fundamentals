69a8d600a4fc13c9556e3256049ff75f01435bf4
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-26 23:52:35 +0330

    Update question 130

--- src/P130.java
+++ src/P130.java
@@ -1,2 +1,30 @@
-public class P130 { 
+
+import java.util.Scanner;
+
+public class P130 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter Number : ");
+        long n = input.nextLong();
+
+        int A[] = new int[100];
+        int c = 0;
+
+        while (n > 0) {
+
+            A[c] = (int) (n % 2);
+            c++;
+
+            n /= 2;
+        }
+
+        System.out.print("Binary is : ");
+
+        for (int x = c - 1; x >= 0; x--) {
+            System.out.print(A[x]);
+        }
+    }
 }
