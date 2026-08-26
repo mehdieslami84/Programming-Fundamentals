957651778c23b8b0dac5ee85a01a7534ad760451
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-08-26 22:04:35 +0330

    Add question 123

---
+++ src/P123.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+// Write a program that takes 5 numbers and prints them from last to first.
+public class P123 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int A[] = new int[20];
+        int sum = 0;
+
+        for (int x = 0; x < 20; x++) {
+            System.out.print("Enter Number " + (x + 1) + " : ");
+            A[x] = input.nextInt();
+            sum += A[x];
+
+        }
+        double avg = sum / 20;
+        System.out.println("avg is : " + avg);
+        for (int x = 0; x < 20; x++) {
+            if (A[x] > avg) {
+                System.out.print(A[x] + "  ");
+            }
+        }
+
+    }
+}
