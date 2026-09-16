7e7f88bd2f73c759ad9112294f1a04a224e6bb5c
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-09-16 22:48:29 +0330

    Add question P102

---
+++ src/P102.java
@@ -0,0 +1,13 @@
+
+public class P102 {
+
+    public static void main(String[] args) {
+
+        int s = 0;
+
+        for (int x = 1; x <= 100; x++) {
+            s += x;
+        }
+        System.out.println(s);
+    }
+}
