f91673632d925634e66a23dfe0b141b413ffe830
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-09-16 23:00:49 +0330

    Add question P105

---
+++ src/P105.java
@@ -0,0 +1,13 @@
+
+public class P105 {
+
+    public static void main(String[] args) {
+
+        double s = 0;
+        for (int x = 1; x <= 100; x++) {
+            s += 1.0 / x;
+
+        }
+        System.out.println(s);
+    }
+}
