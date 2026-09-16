ca1115ecdbe0505f4fe26a4f9a275a6931a0a688
mehdi-eslamifar84 <mm.esf84@gmail.com>
2026-09-16 23:37:13 +0330

    Add question P109

---
+++ src/P109.java
@@ -0,0 +1,16 @@
+
+public class P109 {
+
+    public static void main(String[] args) {
+
+        double s = 0;
+        double p = 1;
+        for (int x = 1; x <= 100; x += 2) {
+
+            p = p * (x);
+            s += 1.0 / p;
+
+        }
+        System.out.println(s);
+    }
+}
